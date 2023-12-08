7214cbf5a185f8da33c3a193e49a6c8316b303c1
uladzimir <01myit@gmail.com>
2023-12-08 20:57:25 +0300

    I used composeWithDevTools from @redux-devtools/extension.

--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,13 @@
 import { legacy_createStore, combineReducers, applyMiddleware } from 'redux'
 import reducerCounter from './counterSlice'
 import reducerArticle from './articleSlice'
+import { composeWithDevTools } from '@redux-devtools/extension'
 const rootReducer = combineReducers({
   counter: reducerCounter,
   article: reducerArticle
 })
-const customStringMiddleware = () => (next) => (action) => {      // the store is the first function ({ dispatch, getState })
-  return typeof action === 'string' ? next({ type: action }) : next(action) // next is a dispatch
-}
-//window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), 
-const enhancers = applyMiddleware( customStringMiddleware )
-const store = legacy_createStore( rootReducer, enhancers )
+const stringCustomMiddleware = storeAPI => wrapDispatch => handleAction => { //! storeAPI({ dispatch, getState }) 
+  return typeof handleAction === 'string' ? wrapDispatch({ type: handleAction }) : wrapDispatch(handleAction)
+} 
+const store = legacy_createStore(rootReducer, composeWithDevTools(applyMiddleware(stringCustomMiddleware)) )
 export default store
