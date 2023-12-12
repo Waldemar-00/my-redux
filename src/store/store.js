3025be8629226483d541a0ba4c50f8228cc39e72
uladzimir <01myit@gmail.com>
2023-12-12 19:51:55 +0300

    I used configureStore to create a store.

--- src/store/store.js
+++ src/store/store.js
@@ -1,18 +1,27 @@
-import { legacy_createStore, combineReducers, applyMiddleware } from 'redux'
+// import { legacy_createStore, combineReducers, applyMiddleware } from 'redux'
 import reducerCounter from './counterSlice'
 import reducerArticle from './articleSlice'
 import reducerTodo from './todoSlice'
 import reducerHeroes from './heroesSlice'
-import { composeWithDevTools } from '@redux-devtools/extension'
-import { thunk } from 'redux-thunk'
-const rootReducer = combineReducers({
-  counter: reducerCounter,
-  article: reducerArticle,
-  todo: reducerTodo,
-  heroes: reducerHeroes
+import { configureStore } from '@reduxjs/toolkit'
+// import { composeWithDevTools } from '@redux-devtools/extension'
+// import { thunk } from 'redux-thunk'
+// const rootReducer = combineReducers({
+  // counter: reducerCounter,
+  // article: reducerArticle,
+  // todo: reducerTodo,
+  // heroes: reducerHeroes
+// })
+// const stringCustomMiddleware = storeAPI => wrapDispatch => handleAction => { //! storeAPI({ dispatch, getState }) 
+  // return typeof handleAction === 'string' ? wrapDispatch({ type: handleAction }) : wrapDispatch(handleAction)
+// } 
+// const store = legacy_createStore(rootReducer, composeWithDevTools(applyMiddleware(stringCustomMiddleware, thunk)) )
+const store = configureStore({ //!also added: thunk+, composeWithDevTools 
+  reducer: {
+    counter: reducerCounter,
+    article: reducerArticle,
+    todo: reducerTodo,
+    heroes: reducerHeroes
+  }
 })
-const stringCustomMiddleware = storeAPI => wrapDispatch => handleAction => { //! storeAPI({ dispatch, getState }) 
-  return typeof handleAction === 'string' ? wrapDispatch({ type: handleAction }) : wrapDispatch(handleAction)
-} 
-const store = legacy_createStore(rootReducer, composeWithDevTools(applyMiddleware(stringCustomMiddleware, thunk)) )
 export default store
