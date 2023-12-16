50e42b50f70a6ddfb2d03160b64ec33db07751b4
uladzimir <01myit@gmail.com>
2023-12-16 15:30:35 +0300

    The Create callback notation was utilized by me to create reducer.

--- src/store/counterSlice.js
+++ src/store/counterSlice.js
@@ -3,9 +3,11 @@ const initialState = { counter: 17 }
 const counterSlice = createSlice({
   name: 'reducerCounter',
   initialState,
-  reducers: {
-    counter_increment: (state, action) => { state.counter = state.counter + action.payload }
-  }
-})
+  reducers: (create) => ({ //! 'create callback' notation
+      counter_increment: create.preparedReducer( number => ({ payload: number * 2 }),
+      (state, action) => { state.counter = state.counter + action.payload }
+      ), 
+    })
+  })
 export const { counter_increment } = counterSlice.actions
 export default counterSlice.reducer
